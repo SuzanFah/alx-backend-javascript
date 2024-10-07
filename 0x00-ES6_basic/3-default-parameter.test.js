64dbeabe4fec78f36166bbb634d557e57d9d8932
SuzanFah <susiefahim@gmail.com>
2024-10-07 20:58:25 +0000

    test1

--- 0x00-ES6_basic/3-default-parameter.test.js
+++ 0x00-ES6_basic/3-default-parameter.test.js
@@ -1,5 +1,13 @@
 import getSumOfHoods from './3-default-parameter.js';
 
-test('returns the new number of neighborhoods', () => {
-  expect(getSumOfHoods(10)).toBe(118);
+test('returns correct sum with default values', () => {
+  expect(getSumOfHoods(34)).toBe(142); // 34 + 89 + 19
+});
+
+test('returns correct sum when passing second parameter', () => {
+  expect(getSumOfHoods(34, 3)).toBe(56); // 34 + 3 + 19
+});
+
+test('returns correct sum when passing both parameters', () => {
+  expect(getSumOfHoods(34, 3, 4)).toBe(41); // 34 + 3 + 4
 });
