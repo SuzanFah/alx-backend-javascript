4d49fde47ee70df5726686c170210dc7d8e58133
SuzanFah <susiefahim@gmail.com>
2024-10-07 15:52:37 +0000

    parameters

---
+++ 0x00-ES6_basic/3-main.js
@@ -0,0 +1,6 @@
+import getSumOfHoods from './3-default-parameter.js';
+
+console.log(getSumOfHoods(34));          // Expected Output: 142 (34 + 89 + 19)
+console.log(getSumOfHoods(34, 3));       // Expected Output: 56 (34 + 3 + 19)
+console.log(getSumOfHoods(34, 3, 4));    // Expected Output: 41 (34 + 3 + 4)
+
